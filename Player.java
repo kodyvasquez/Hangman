8ea351187123965e633e515af24f1af1b7daa8b0
Kody Vasquez <kody.vasquez@student.tamdistrict.org>
2025-04-27 14:48:07 -0700

    first class completed

--- Player.java
+++ Player.java
@@ -1,21 +1,3 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Player {
 
-    public class player {
-        private Set<Character> guesses;
-
-        public player() {
-            guesses = new HashSet<>();
-        }
-
-        public boolean hasGuessed(char letter) {
-            return guesses.contains(letter);
-        }
-
-        public void addGuess(char letter) {
-            guesses.add(letter);
-        }
-    }
 }
--- Secret.java
+++ Secret.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
+
 public class Secret {
-    
+    private String word;
+    private char[] revealedWord;
+
+    public Secret() {
+        String[] wordList = { "hangman", "java", "programming", "developer", "openai" };
+        word = wordList[(int) (Math.random() * wordList.length)];
+        revealedWord = new char[word.length()];
+        Arrays.fill(revealedWord, '_');
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getRevealedWord() {
+        return new String(revealedWord);
+    }
+
+    public boolean checkGuess(char guess) {
+        boolean correct = false;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess) {
+                revealedWord[i] = guess;
+                correct = true;
+            }
+        }
+        return correct;
+    }
+
+    public boolean isSolved() {
+        return word.equals(getRevealedWord());
+    }
 }
